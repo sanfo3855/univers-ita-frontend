8fbc5ade8b6a84270580d48a90e883d4425529ed
Matteo Sanfelici <sanfelicimatteo@gmail.com>
2020-11-18 11:31:43 +0100

    fix . in open response

--- src/app/@core/components/question/question.component.ts
+++ src/app/@core/components/question/question.component.ts
@@ -151,7 +151,6 @@ export class QuestionComponent implements /*OnInit,*/ OnChanges {
         if (!this.responses) {
           this.responses = [];
         }
-        new_response = new_response.replace("."," ")
       } else {
         this.responses = this.responses.filter((val, i, arr) => {
           if (val !== new_response) {
